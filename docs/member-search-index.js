25845406c9110d714725e449c052c2cfdea476c3
IvanRGGS <120422627+IvanRGGS@users.noreply.github.com>
2023-03-01 16:49:30 +0100

    Add files via upload

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"calcualdora","c":"CalculadoraED","l":"CalculadoraED()","u":"%3Cinit%3E()"},{"p":"calcualdora","c":"CalculadoraED","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
